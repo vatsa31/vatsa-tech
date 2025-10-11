b57b1a150a00a30368d5e0f87b0f5de9aafc499b
Shrivatsa Kashyap <shrivatsakashyap@Shrivatsas-MacBook-Pro.local>
2025-10-11 12:13:45 +0530

    chore - update next metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,16 +12,17 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://nim-fawn.vercel.app/'),
+  metadataBase: new URL('https://vatsa-tech.vercel.app/'),
   alternates: {
-    canonical: '/'
+    canonical: '/',
   },
   title: {
-    default: 'Nim - Personal website template',
-    template: '%s | Nim'
+    default: 'Shrivatsa Kashyap',
+    template: '%s | Shrivatsa',
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
-};
+  description:
+    'Portfolio website of Shrivatsa Kashyap built with Nim in NextJS',
+}
 
 const geist = Geist({
   variable: '--font-geist',
